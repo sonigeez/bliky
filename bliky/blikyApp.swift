7ab26d2c94cdd47f8497e904c356eb2f5388d999
sonigeez <bharatchills@gmail.com>
2024-11-29 04:04:40 +0530

    Initial Commit

---
+++ bliky/blikyApp.swift
@@ -0,0 +1,17 @@
+//
+//  blikyApp.swift
+//  bliky
+//
+//  Created by bharat on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct blikyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blikyTests/blikyTests.swift
@@ -0,0 +1,17 @@
+//
+//  blikyTests.swift
+//  blikyTests
+//
+//  Created by bharat on 29/11/24.
+//
+
+import Testing
+@testable import bliky
+
+struct blikyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
